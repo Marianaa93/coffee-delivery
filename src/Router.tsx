b4438db58df0a04e39f7a9aea9ab1cb8537c946f
Mariana Maia <marianalima.amb@gmail.com>
2024-03-20 01:26:01 -0300

    creates default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,24 +2,30 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Checkout } from "./pages/Checkout";
 import { SuccessPage } from "./pages/SuccessPage";
+import { DefaultLayout } from "./Layouts/defaultLayout";
 
 export function Router() {
   return (
     <Routes>
       <Route
         path='/'
-        element={<Home />}
-      />
+        element={<DefaultLayout />}
+      >
+        <Route
+          path='/'
+          element={<Home />}
+        />
 
-      <Route
-        path='/checkout'
-        element={<Checkout />}
-      />
+        <Route
+          path='/checkout'
+          element={<Checkout />}
+        />
 
-      <Route
-        path='/succes'
-        element={<SuccessPage />}
-      />
+        <Route
+          path='/succes'
+          element={<SuccessPage />}
+        />
+      </Route>
     </Routes>
   );
 }
