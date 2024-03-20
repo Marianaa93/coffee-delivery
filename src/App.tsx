b9ea752e6c89194130d3ea42b20109e972f833d7
Mariana Maia <marianalima.amb@gmail.com>
2024-03-20 01:23:20 -0300

    creates Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
-import { Header } from "./components/Header";
 import { defaultTheme } from "./styles/themes/defaultTheme";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Header />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
