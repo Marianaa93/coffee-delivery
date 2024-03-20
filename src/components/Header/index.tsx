fd87eca18bdce801d92d8531f7356facba1acd03
Mariana Maia <marianalima.amb@gmail.com>
2024-03-20 00:45:02 -0300

    creates default theme colors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,1 +1,14 @@
-export function Header() {}
+import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import logo from "../../assets/logo.svg";
+
+export function Header() {
+  return (
+    <div>
+      <img src={logo} />
+      <div>
+        <MapPin /> <p>Porto Alegre</p>
+      </div>
+      <ShoppingCart size={24} />
+    </div>
+  );
+}
